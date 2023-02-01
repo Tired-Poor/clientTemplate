a7b6f93fec74992ab6c8ed4018e7368c0b5ed110
Andrew Cook <andrewjcook93@gmail.com>
2023-02-01 00:44:04 -0600

    added footer component, changed App.js to Outlet Context ONLY

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar-return">
-      NAVBAR
+      <Link to="/">Home</Link>
+      <Link to="about">About</Link>
     </div>
   )
 }
+
+export default Navbar;
