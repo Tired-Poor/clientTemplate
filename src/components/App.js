91b81df8e08204fb94fc163a9c9aea5d5eb8109b
Andrew Cook <andrewjcook93@gmail.com>
2023-01-31 23:51:32 -0600

    toml, outlet context, added navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import { useOutletContext } from 'react-router-dom';
+import './style.css';
+
 const App = () => {
   return(
     <div>
       App PAGE
+      <header>TEMPLATE Header</header>
+
+      <Outlet context />
+
+
     </div>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="navbar-return">
+      NAVBAR
+    </div>
+  )
+}
