acc462ae7fbf9e679d4c5a1b48c106c863ca76c1
Andrew Cook <andrewjcook93@gmail.com>
2023-01-31 23:38:11 -0600

    basic router, missing components

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import {createRoot} from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import App from './components/App';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import About from './components/About';
+
+
+const appElement = document.getElementById('app');
+const root = createRoot(appElement);
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        index: "about",
+        element: <About />
+      }
+    ]
+  }
+]);
+
+root.render(<RouterProvider router = {router} />);
