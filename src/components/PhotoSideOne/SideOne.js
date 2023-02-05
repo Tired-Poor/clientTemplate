fb8f021c21ebd3fdf007071247138f4d5b7482c9
Andrew Cook <andrewjcook93@gmail.com>
2023-02-05 14:10:59 -0600

    partial sidenav functionality-animations only. need hidden func

--- src/components/PhotoSideOne/SideOne.js
+++ src/components/PhotoSideOne/SideOne.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 // import Footer from '../Footer';
-import './style.css';
+import './sideone.css';
 
 // Inspiration https://jenhuangphoto.com/#
 
 const SideOne = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [weddingsClass, setWeddingsClass] = useState("weddings hidden")
+
+
+  const controlNavbar = () => {
+    if (!isClicked) {
+      setWeddingsClass("weddings visible")
+    }
+    else {
+      setWeddingsClass("weddings hidden")
+    }
+  }
+
+
   return (
     <div className="side-one-return">
       <div className="sideone-sidebar-main-flexbox">
@@ -16,13 +31,55 @@ const SideOne = () => {
           <Link to="/">&#x2190; Return Home</Link>
           {/* <Link to="side-one">Drew's Sidebar page</Link> */}
           <section className="sideone-link-cats">
-            <span className="sideone-link-cat">WEDDINGS + </span><br />
-            <span className="sideone-link-cat">ENGAGEMENTS + </span><br />
-            <span className="sideone-link-cat">GRADUATION + </span><br />
-            <span className="sideone-link-cat">NATURE + </span><br />
-            <span className="sideone-link-cat">CREATIVE + </span><br />
-            <span className="sideone-link-cat">JOURNAL + </span><br />
-            <span className="sideone-link-cat">CONTACT </span><br />
+            <span onClick={isClicked} className="sideone-link-cats__cat">WEDDINGS + </span>
+            <ul className="category weddings hidden">
+              <li className="category__item">Bill & Frank, 2023</li>
+              <li className="category__item">Harry & Megan, 2018</li>
+              <li className="category__item">Jim & Pam, 2014</li>
+              <li className="category__item">Ross & Rachel 2002</li>
+            </ul><br />
+
+            <span className="sideone-link-cats__cat">ENGAGEMENTS + </span><br />
+            <ul className="category engagements hidden">
+              <li className="category__item">Bill & Frank, 2023</li>
+              <li className="category__item">Harry & Megan, 2018</li>
+              <li className="category__item">Jim & Pam, 2014</li>
+              <li className="category__item">Ross & Rachel 2002</li>
+            </ul><br />
+
+            <span className="sideone-link-cats__cat">GRADUATION + </span><br />
+            <ul className="category graduation hidden">
+              <li className="category__item">Bill & Frank, 2023</li>
+              <li className="category__item">Harry & Megan, 2018</li>
+              <li className="category__item">Jim & Pam, 2014</li>
+              <li className="category__item">Ross & Rachel 2002</li>
+            </ul><br />
+
+            <span className="sideone-link-cats__cat">NATURE + </span><br />
+            <ul className="category nature hidden">
+              <li className="category__item">Bill & Frank, 2023</li>
+              <li className="category__item">Harry & Megan, 2018</li>
+              <li className="category__item">Jim & Pam, 2014</li>
+              <li className="category__item">Ross & Rachel 2002</li>
+            </ul><br />
+
+            <span className="sideone-link-cats__cat">CREATIVE + </span><br />
+            <ul className="category hidden">
+              <li className="category__creative">Bill & Frank, 2023</li>
+              <li className="category__creative">Harry & Megan, 2018</li>
+              <li className="category__creative">Jim & Pam, 2014</li>
+              <li className="category__creative">Ross & Rachel 2002</li>
+            </ul><br />
+
+            <span className="sideone-link-cats__cat">JOURNAL + </span><br />
+            <ul className="category journal hidden">
+              <li className="category__item">Bill & Frank, 2023</li>
+              <li className="category__item">Harry & Megan, 2018</li>
+              <li className="category__item">Jim & Pam, 2014</li>
+              <li className="category__item">Ross & Rachel 2002</li>
+            </ul><br />
+
+            <span className="sideone-link-cats__cat">CONTACT </span><br />
           </section>
         </nav>
         <main>
