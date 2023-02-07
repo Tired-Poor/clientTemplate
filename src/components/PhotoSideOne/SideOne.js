e52d75cbd62308874214eb184d11f44da6b457da
Andrew Cook <andrewjcook93@gmail.com>
2023-02-06 19:43:26 -0600

    sideone. added different info to side nav

--- src/components/PhotoSideOne/SideOne.js
+++ src/components/PhotoSideOne/SideOne.js
@@ -132,47 +132,52 @@ const SideOne = () => {
               <li className="category__item">Bill & Frank, 2023</li>
               <li className="category__item">Harry & Megan, 2018</li>
               <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+              <li className="category__item">Ross & Rachel, 2002</li>
+              <li className="category__item">Denise & Dwight, 2000</li>
             </ul><br /><br />
 
             <span onClick = {controlEngagements } className="sideone-link-cats__cat">ENGAGEMENTS + </span><br />
             <ul className={engagementsClass}>
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+              <li className="category__item">Liz and Criss</li>
+              <li className="category__item">Emily and John</li>
+              <li className="category__item">Bill and Ted</li>
+              <li className="category__item">Christina and Everett</li>
             </ul><br />
 
             <span onClick={controlGraduations} className="sideone-link-cats__cat">GRADUATION + </span><br />
             <ul className={graduationsClass}>
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+              <li className="category__item">Jiliian, Texas A&M</li>
+              <li className="category__item">Christopher, UT Austin</li>
+              <li className="category__item">Tyler SHSU</li>
+              <li className="category__item">Robyn Texas State</li>
             </ul><br />
 
             <span onClick={controlNatures} className="sideone-link-cats__cat">NATURE + </span><br />
             <ul className={naturesClass}>
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+              <li className="category__item">Houston Zoo</li>
+              <li className="category__item">Dallas Zoo</li>
+              <li className="category__item">African Safari</li>
+              <li className="category__item">Rio Grande Valley</li>
             </ul><br />
 
             <span onClick={controlCreatives} className="sideone-link-cats__cat">CREATIVE + </span><br />
             <ul className={creativesClass}>
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+              <li className="category__item">Nightlife</li>
+              <li className="category__item">Stars</li>
+              <li className="category__item">Artistic</li>
+              <li className="category__item">Landscapes</li>
             </ul><br />
 
             <span onClick={controlJournals} className="sideone-link-cats__cat">JOURNAL + </span><br />
             <ul className={journalsClass}>
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+              <li className="category__item">Apr, 2023</li>
+              <li className="category__item">Nov, 2022</li>
+              <li className="category__item">Feb, 2022</li>
+              <li className="category__item">Dec, 2021</li>
+              <li className="category__item">Aug, 2021</li>
+              <li className="category__item">Jan, 2021</li>
+              <li className="category__item">Sept, 2020</li>
+              <li className="category__item">Mar, 2020</li>
             </ul><br />
 
             <span className="sideone-link-cats__cat">CONTACT </span><br />
