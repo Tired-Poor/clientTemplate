3843925652052b8143126b731dd02d873cf3b26f
Andrew Cook <andrewjcook93@gmail.com>
2023-02-07 19:40:08 -0600

    sideone dropdown nav functionality

--- src/components/PhotoSideOne/SideOneDropdown.js
+++ src/components/PhotoSideOne/SideOneDropdown.js
@@ -1,56 +1,55 @@
 import React from 'react';
-import './dropdown.css';
 
 function SideOneDropdown() {
   return (
     <div className="sideone-dropdown-return">
       <nav className="sideone-dropdown-nav">
             <span className="sideone-link-cats__cat">WEDDINGS</span>
-            <ul className="category">
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+            <ul className="category category-mobile">
+              <li className="category__item sideone-mobile-nav-item">Bill & Frank, 2023</li>
+              <li className="category__item sideone-mobile-nav-item">Harry & Megan, 2018</li>
+              <li className="category__item sideone-mobile-nav-item">Jim & Pam, 2014</li>
+              <li className="category__item sideone-mobile-nav-item">Ross & Rachel 2002</li>
             </ul><br />
 
             <span className="sideone-link-cats__cat">ENGAGEMENTS</span><br />
-            <ul className="category">
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+            <ul className="category category-mobile">
+              <li className="category__item sideone-mobile-nav-item">Bill & Frank, 2023</li>
+              <li className="category__item sideone-mobile-nav-item">Harry & Megan, 2018</li>
+              <li className="category__item sideone-mobile-nav-item">Jim & Pam, 2014</li>
+              <li className="category__item sideone-mobile-nav-item">Ross & Rachel 2002</li>
             </ul><br />
 
             <span className="sideone-link-cats__cat">GRADUATION</span><br />
-            <ul className="category">
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+            <ul className="category category-mobile">
+              <li className="category__item sideone-mobile-nav-item">Bill & Frank, 2023</li>
+              <li className="category__item sideone-mobile-nav-item">Harry & Megan, 2018</li>
+              <li className="category__item sideone-mobile-nav-item">Jim & Pam, 2014</li>
+              <li className="category__item sideone-mobile-nav-item">Ross & Rachel 2002</li>
             </ul><br />
 
             <span className="sideone-link-cats__cat">NATURE</span><br />
-            <ul className="category">
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+            <ul className="category category-mobile">
+              <li className="category__item sideone-mobile-nav-item">Bill & Frank, 2023</li>
+              <li className="category__item sideone-mobile-nav-item">Harry & Megan, 2018</li>
+              <li className="category__item sideone-mobile-nav-item">Jim & Pam, 2014</li>
+              <li className="category__item sideone-mobile-nav-item">Ross & Rachel 2002</li>
             </ul><br />
 
             <span className="sideone-link-cats__cat">CREATIVE</span><br />
-            <ul className="category">
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+            <ul className="category category-mobile">
+              <li className="category__item sideone-mobile-nav-item">Bill & Frank, 2023</li>
+              <li className="category__item sideone-mobile-nav-item">Harry & Megan, 2018</li>
+              <li className="category__item sideone-mobile-nav-item">Jim & Pam, 2014</li>
+              <li className="category__item sideone-mobile-nav-item">Ross & Rachel 2002</li>
             </ul><br />
 
             <span className="sideone-link-cats__cat">JOURNAL</span><br />
-            <ul className="category">
-              <li className="category__item">Bill & Frank, 2023</li>
-              <li className="category__item">Harry & Megan, 2018</li>
-              <li className="category__item">Jim & Pam, 2014</li>
-              <li className="category__item">Ross & Rachel 2002</li>
+            <ul className="category category-mobile">
+              <li className="category__item sideone-mobile-nav-item">Bill & Frank, 2023</li>
+              <li className="category__item sideone-mobile-nav-item">Harry & Megan, 2018</li>
+              <li className="category__item sideone-mobile-nav-item">Jim & Pam, 2014</li>
+              <li className="category__item sideone-mobile-nav-item">Ross & Rachel 2002</li>
             </ul><br />
 
             <span className="sideone-link-cats__cat">CONTACT</span><br />
